ea1c5facbe2a778024d3481c139aec5dd6f4886a
Daniel Fuller <danielfuller@Daniels-MacBook-Pro.local>
2015-04-04 22:51:17 -0400

    copying files from PhoneGap Assignment 4

---
+++ JS/Assignment4.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#areaDropdown").change(function() {
+        var selectedIndex = $("#areaDropdown option:selected").index();
+        
+        if (selectedIndex == "0") {
+                $("#area1Wrapper").toggle(false);
+                $("#area2Wrapper").toggle(false);
+        }
+        
+        else if(selectedIndex == "1"){
+            $("#area2Wrapper").toggle(false);
+            $("#area1Wrapper").toggle(true);
+        }
+        
+        else if(selectedIndex == "2"){
+            $("#area1Wrapper").toggle(false);
+            $("#area2Wrapper").toggle(true);
+        }
+    });
+});
